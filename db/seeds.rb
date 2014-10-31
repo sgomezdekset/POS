9e45456bc45dc934decd6e800559a61e73e5e0fa
Fer Martin <fernandomartinsantana@gmail.com>
2014-10-31 12:24:04 +0800

    Initial seed data - to be continued

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+starter   = Menu.create name: 'Starters'
+mains     = Menu.create name: 'Mains'
+desserts  = Menu.create name: 'Desserts'
+softdrink = Menu.create name: 'Soft Drinks'
+alcohol   = Menu.create name: 'Alcohol'
+
+Item.create name: 'Green Salad', price: 50 , menu: starter
+Item.create name: 'Raw Carrots', price: 30 , menu: starter
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
+# Item.create name: '', price:  , menu:
